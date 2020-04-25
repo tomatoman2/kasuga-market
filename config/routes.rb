f873dcaea25e6772b1a146ca204aa81d3a1f3bdc
tomatoman2 <kasuga01m@gmail.com>
2020-04-25 12:05:00 +0900

    add-show-action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   root to: "home#index"
+  resources :items, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
