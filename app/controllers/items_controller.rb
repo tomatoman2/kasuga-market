4a01d6b574072b1a010333d20f012967b514082c
tomatoman2 <kasuga01m@gmail.com>
2020-04-28 14:43:41 +0900

    Add:出品詳細機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,8 @@
 class ItemsController < ApplicationController
   def show
     @products = Product.all
-    @products = ProductImage.all
-    @product = ProductImage.find(params[:id])
     @product = Product.find(params[:id]) 
-    # @categories = Category.all
-    # @category = Category.find(category_id)
+    @images = ProductImage.all
+    @image = ProductImage.find(params[:id])
   end 
 end
