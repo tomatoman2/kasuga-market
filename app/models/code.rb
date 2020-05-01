09ea6454e715c249e626c6aa320e0f065d49305e
tomatoman2 <kasuga01m@gmail.com>
2020-05-01 21:23:32 +0900

    Fix:モデルの修正・他日本語設定

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,7 +1,18 @@
-class Code < ApplicationRecord
-  belongs_to :product
-  validates :group_code_id, presence: true
-  validates :group_code_name, presence: true
-  validates :code_id, presence: true
-  validates :code_name, presence: true
+class Code < ActiveHash::Base
+  self.data = [
+      {id:1, group_id:100, group_name:"商品の状態", code_id:0, code_name:"新品、未使用"},
+      {id:2, group_id:100, group_name:"商品の状態", code_id:1, code_name:"未使用に近い"},
+      {id:3, group_id:100, group_name:"商品の状態", code_id:2, code_name:"目立った傷や汚れなし"},
+      {id:4, group_id:100, group_name:"商品の状態", code_id:3, code_name:"やや傷や汚れあり"},
+      {id:5, group_id:100, group_name:"商品の状態", code_id:4, code_name:"傷や汚れあり"},
+      {id:6, group_id:100, group_name:"商品の状態", code_id:5, code_name:"全体的に状態が悪い"},
+      {id:7, group_id:101, group_name:"配送料の負担", code_id:0, code_name:"送料込み（出品者負担）"},
+      {id:8, group_id:101, group_name:"配送料の負担", code_id:1, code_name:"着払い（購入者負担）"},
+      {id:9, group_id:102, group_name:"発送までの日数", code_id:0, code_name:"1~2日で発送"},
+      {id:10, group_id:102, group_name:"発送までの日数", code_id:1, code_name:"2~3日で発送"},
+      {id:11, group_id:102, group_name:"発送までの日数", code_id:2, code_name:"4~7日で発送"}
+  ]
+  def self.group_search(group_id)
+    Code.where(group_id: group_id)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module KasugaMarket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja #jaに変更
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
